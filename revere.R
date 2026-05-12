efff647344c31bcdb9ec2af6611600b2832f7e55
mmcgowan1213 <matthewmcgowan1213@gmail.com>
2026-05-12 10:06:32 -0400

    Update revere.R

--- revere.R
+++ revere.R
@@ -9,8 +9,8 @@ RevereCareer <- Batting %>%
                  SLG = (singles + X2B * 2 + X3B * 3 + HR *4)/ AB)  
 
 ggplot(RevereCareer, aes(x = yearID,
-           y = AVG,
-           color = AVG)) +
+                         y = AVG,
+                         color = AVG)) +
   geom_line()
 
 ggplot(RevereCareer, aes(x = yearID,
